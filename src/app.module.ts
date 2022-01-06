a5af8ae30e015bccefc9ed3e89bfaec3615b2326
LucasJS331 <69942512+LucasJS331@users.noreply.github.com>
2022-01-06 19:14:14 -0300

    update ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DesafiosModule } from './desafios/desafios.module';
 import { PartidasModule } from './partidas/partidas.module';
 import { ProxyRMQModule } from './proxyrmq/proxyrmq.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule,ConfigService } from '@nestjs/config';
+
+const config_service = new ConfigService();
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin_sr:kdWcpu1jWvUSlU6Y@clustermogodb-79l5n.mongodb.net/srdesafios?retryWrites=true&w=majority',
+    MongooseModule.forRoot(config_service.get("MONGO_URI"),
       { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
     DesafiosModule,
     PartidasModule,
